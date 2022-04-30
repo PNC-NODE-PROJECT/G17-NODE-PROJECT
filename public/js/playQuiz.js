fc360cd64586e998980c75de9c3daedbff8ba26e
Huymey.KUM <Huymey.KUM@pnc.lan>
2022-05-01 00:18:39 +0700

    #34 get data from backend

--- public/js/playQuiz.js
+++ public/js/playQuiz.js
@@ -1,39 +1,19 @@
 // fake data 
-let data =[
-    {
-        question:"What does CSS stand for?",
-        a: "Cascading Style Sheets",
-        b: "Cascading Simple Sheets",
-        c:"Central Style Sheets",
-        correct:"a",
-    },
-    {
-        question: "What does HTML stand for?",
-        a: "Hypertext Markdown Language",
-        b: "Hyperloop Machine Language",
-        c: "Hypertext Markup Language",
-        correct:"c",
-    },
-    {
-        question: "What does URL stand for?",
-        a: "Place to search something",
-        b: "Uniform Resource Locator.",
-        c: "rearch tool",
-        correct:"b",
-    },
-];
+var data =[];
 
-// function getData() {
-//     axios.get('/api/getQuestions').then(response => {
-//     let getData = response.data;
-//     refreshDom(getData);
-// });
-// }
+
+axios.get('/api/getQuestions').then(response => {
+    data = response.data;
+    getData(data);
+})
+function getData(data) {
+    for (let index = 0; index < data.length; index++) {
 
 
 
-function refreshDom(getData) {
-    for (let index = 0; index < getData.length; index++) {
+
+
+
         let container = document.createElement('div');
         container.className = document.querySelector('.quiz-container');
         let quiz_header = document.createElement('div');
@@ -43,36 +23,30 @@ function refreshDom(getData) {
 
 
         // appendChild---------------
-        question.appendChild(quiz-header);
+        question.appendChild(quiz_header);
         quiz_header.appendChild(container);
     }
-};
+}
 
-// const quizData = []
-// axios.get('/api/getQuestions').then(response => {
-//     let quizData = response.data;
-//     console.log(quizData);
-// });
 
-const quiz= document.getElementById('quiz')
+
+const quiz= document.getElementById('quiz');
+
+
+
 const answerEls = document.querySelectorAll('.answer')
 const questionEl = document.getElementById('question')
 const a_text = document.getElementById('a_text')
 const b_text = document.getElementById('b_text')
 const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
 const submitBtn = document.getElementById('submit')
-
+var allQuestionScore = 0;
 
 let currentQuiz = 0
 let score = 0
 
-// let getData = getdataFromback()
 
-// function getdataFromback(){
-//     axios.get("/",(req,res) =>{
-//         let date = res.data;
-//     })
-// }
 
 
 loadQuiz()
@@ -80,13 +54,16 @@ loadQuiz()
 
 function loadQuiz() {
     deselectAnswers()
-    const currentQuizData = data[currentQuiz]
-
-    questionEl.textContent = currentQuizData.question
-    console.log(currentQuizData);
-    a_text.textContent = currentQuizData.a
-    b_text.textContent = currentQuizData.b
-    c_text.textContent = currentQuizData.c
+    axios.get('/api/getQuestions').then(response => {
+        data = response.data
+        let currentQuizData = data[currentQuiz]
+        questionEl.textContent = currentQuizData.question
+        // console.log(currentQuizData);
+        a_text.textContent = currentQuizData.answer.a
+        b_text.textContent = currentQuizData.answer.b
+        c_text.textContent = currentQuizData.answer.c
+        d_text.textContent = currentQuizData.answer.d
+    })
     
 }
 
@@ -106,30 +83,33 @@ function getSelected() {
 
 
 submitBtn.addEventListener('click', () => {
-    const answer = getSelected()
-    if(answer) {
-       if(answer === data[currentQuiz].correct) {
-           score++
-       }
-
-       currentQuiz++
-
-       if(currentQuiz < data.length) {
-           loadQuiz()
-       } else {
-           quiz.innerHTML = `
-           <h2>You answered ${score}/${data.length} questions correctly</h2>
-           <button onclick="location.reload()">Reload</button>
-           `
-       }
-    }
+    axios.get('/api/getQuestions').then(response => {
+        data = response.data;
+        const answer = getSelected()
+        allQuestionScore+=data[currentQuiz].score
+        if(answer) {
+           if(answer === data[currentQuiz].correctAnswer) {
+               score+= data[currentQuiz].score
+           }
+    
+           currentQuiz++
+    
+           if(currentQuiz < data.length) {
+               loadQuiz()
+           } else {
+               quiz.innerHTML = `
+               <h2>You answered ${parseInt(score)}/${parseInt(allQuestionScore)} scores</h2>
+               <button onclick="location.reload()">Reload</button>
+               `
+           }
+        }
+    })
 })
 
 function showFirstPage(){
     document.querySelector('.container').style.display="block";
     document.querySelector('.container').style.display="flex";
     document.querySelector('.container').style.justifyContent="space-around";
-    // document.querySelector('.container').style.display="block";
     document.querySelector('.navbar').style.display="none";
     document.body.style.backgroundColor ="#d0d624";
     document.body.style.backgroundImage ="none";
